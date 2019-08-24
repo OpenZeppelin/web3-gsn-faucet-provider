2899f3b62bca9c86a867f0d3ac14baabf19fde51
Santiago Palladino <spalladino@gmail.com>
2019-08-24 22:55:52 +0200

    Add singleton deployment info

---
+++ scripts/deployData.js
@@ -0,0 +1,35 @@
+const { Transaction } = require("ethereumjs-tx");
+const ethUtils = require("ethereumjs-util");
+
+const fs = require("fs");
+
+function toHexString(buffer) {
+  return "0x" + buffer.toString("hex");
+}
+
+const { bytecode } = require("../build/contracts/GSNFaucet.json");
+
+const tx = new Transaction({
+  nonce: 0,
+  data: bytecode,
+  value: 0,
+  gasPrice: 100000000000, /// 100 gigawei
+  gasLimit: 500000,
+  v: 27,
+  r: "0x1234567890123456789012345678901234567890123456789012345678901235",
+  s: "0x1234567890123456789012345678901234567890123456789012345678901234"
+});
+
+const deployer = tx.getSenderAddress();
+
+console.log(
+  JSON.stringify({
+    deployer: toHexString(deployer),
+    contract: {
+      address: toHexString(
+        ethUtils.generateAddress(deployer, ethUtils.toBuffer(0))
+      ),
+      deployTx: toHexString(tx.serialize())
+    }
+  })
+);
