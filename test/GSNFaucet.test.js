7c3ba75fd0d0b0fd938e9e3949b5c5190fb728b3
Juan Bautista Carpanelli <juanbanelli@gmail.com>
2019-08-24 18:18:53 +0200

    This is a proper test setup

--- test/GSNFaucet.test.js
+++ test/GSNFaucet.test.js
@@ -2,16 +2,19 @@ const { setupAccounts, deployGSNFaucet } = require("./setup");
 const expect = require("chai").use(require("chai-string")).expect;
 
 describe("GSNFaucet contract", function() {
-  before("setup accounts", async function() {
-    await setupAccounts();
-    console.log(this.sender, this.deployer);
-  });
-
   beforeEach("setup", async function() {
-    await deployGSNFaucet();
+    const accounts = await setupAccounts();
+    this.deployer = accounts[0];
+    this.sender = accounts[1];
+    this.signer = accounts[2];
+    this.failsPre = accounts[7];
+    this.failsPost = accounts[8];
+
+    this.gsnFaucet = await deployGSNFaucet();
   });
 
   it("does something", function() {
-    console.log(this.gsnFaucet.options.address);
+    console.log(this.deployer);
+    console.log(this.gsnFaucet);
   });
 });
--- test/setup.js
+++ test/setup.js
@@ -9,16 +9,13 @@ const PROVIDER_URL = process.env.PROVIDER_URL || "http://localhost:9545";
 
 async function setupAccounts() {
   this.web3 = new Web3(PROVIDER_URL);
-  this.accounts = await this.web3.eth.getAccounts();
-  if (this.accounts.length !== 10) {
-    throw new Error(`Expected 10 accounts but found ${this.accounts}`);
+  const accounts = await this.web3.eth.getAccounts();
+  if (accounts.length !== 10) {
+    throw new Error(`Expected 10 accounts but found ${accounts}`);
   }
+  this.deployer = accounts[0];
 
-  this.deployer = this.accounts[0];
-  this.sender = this.accounts[1];
-  this.signer = this.accounts[2];
-  this.failsPre = this.accounts[7];
-  this.failsPost = this.accounts[8];
+  return accounts;
 }
 
 async function deployGSNFaucet() {
@@ -26,12 +23,13 @@ async function deployGSNFaucet() {
     data: GSNFaucetBytecode
   });
 
-  this.gsnFaucet = await GSNFaucet.deploy().send({
+  const gsnFaucet = await GSNFaucet.deploy().send({
     from: this.deployer,
     gas: 2e6
   });
 
-  await fundRecipient(this.web3, { recipient: this.gsnFaucet.options.address });
+  await fundRecipient(this.web3, { recipient: gsnFaucet.options.address });
+  return gsnFaucet;
 }
 
 module.exports = {
