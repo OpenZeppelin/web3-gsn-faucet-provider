669876f02ca502de0eecfa85c3c15175309fb8b5
Igor Yalovoy <igor@yalovoi.net>
2019-08-24 16:19:03 +0200

    Change GSNFaucet.sol

--- contracts/GSNFaucet.sol
+++ contracts/GSNFaucet.sol
@@ -4,24 +4,56 @@ import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol"
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
 contract GSNFaucet is Initializable, GSNRecipient {
+  uint256 private difficulty;
+  uint256 private lastNonce = 0;
 
-  function initialize() public initializer {
+  function initialize(uint256 _difficulty) public initializer {
     GSNRecipient.initialize();
+    difficulty = _difficulty;
+  }
+
+  function getLastNonce() public view returns (uint256) {
+    return lastNonce;
+  }
+
+  function getDifficulty() public view returns (uint256) {
+    return difficulty;
+  }
+
+  function checkProof(uint256 nonce) public view returns (bool) {
+    bytes32 digest = keccak256(abi.encodePacked(_msgSender(), lastNonce, nonce));
+    require(uint256(digest) < getDifficulty(), "Do more work!");
+    return true;
+  }
+
+  function _preRelayedCall(bytes context) {
+    lastNonce = nonce;
+  }
+
+  function fooBar(uint256 nonce) public {
+
   }
 
   // accept all requests
   function acceptRelayedCall(
-    address,
-    address,
-    bytes calldata,
-    uint256,
-    uint256,
-    uint256,
-    uint256,
-    bytes calldata,
-    uint256
-    ) external view returns (uint256, bytes memory) {
-    return _approveRelayedCall();
+    address relay,
+    address from,
+    bytes calldata encodedFunction,
+    uint256 transactionFee,
+    uint256 gasPrice,
+    uint256 gasLimit,
+    uint256 nonce,
+    bytes calldata approvalData,
+    uint256 maxPossibleCharge
+) external view returns (uint256, bytes memory) {
+    abi.decode();
+    if(this.checkProof()) {
+      return _approveRelayedCall(abi.encodePacked());
+    } else {
+
+    }
+    // lastNonce = nonce;
+
   }
 
   function getRecipientBalance() public view returns (uint) {
